d954d18762c7d67576361f7bbac520b028cbce82
kumbasar <vkumbasar@outlook.com>
2017-07-02 19:46:29 +0300

    first script

---
+++ hello.py
@@ -0,0 +1,31 @@
+print("Hello World!")
+
+name = "Macbook"
+machine = "HAL"
+
+pi = 3.1415
+int(pi)
+
+
+"Nice to meet you {0}. I am {1}".format(name,machine)
+
+
+a = 1
+b = 2
+"bigger" if a > b else "smaller"
+
+number = 5
+if number == 5:
+    print("Number is 5.")
+else:
+    print("Number is not 5.")
+
+
+text = "Python"
+if text:
+    print("text is defined.")
+
+python_course = True
+if not python_course:
+    print("This will not execute.")
+
